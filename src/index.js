14ca147c35740ebbe2f9fcc9b100ac5dd045fd3d
Moisés Ñañez <moisesnandres@gmail.com>
2017-07-02 19:56:39 -0500

    Initialize components and add youtube api key

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDeKTXZ7xypnzJImrzVkp_KReLVwewC6qs';
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
